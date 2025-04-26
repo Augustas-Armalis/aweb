9f190bc0f28b7cabb9e1d83d40a8c08ec1931c2c
Ares <armaugustas@gmail.com>
2025-04-26 19:30:18 +0300

    asdfgh

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+function HomePage() {
+  return (
+    <div className="center-text">
+      <p className="text-2xl font-bold">Hello Augustas</p>
+      <a href="https://www.example.com" target="_blank" rel="noopener noreferrer">
+        <button className="bg-blue-300 font-bold px-5 py-2 mt-3 rounded-xl">
+          Go to Example
+        </button>
+      </a>
+    </div>
+  );
+}
+
+export default HomePage;
