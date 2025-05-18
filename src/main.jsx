f481ba83b6d73efcb67e0cdd2dd20cf2fc1bcfd3
Ares <armaugustas@gmail.com>
2025-05-18 21:48:29 +0300

    will work now!!!

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import './index.css';
 import HomePage from './index.jsx';
@@ -15,25 +15,15 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HelmetProvider>
       <BrowserRouter>
-        <div>
-          <nav>
-            <Link to="/">Home</Link> |{' '}
-            <Link to="/terms">Terms</Link> |{' '}
-            <Link to="/privacy">Privacy</Link> |{' '}
-            <Link to="/newsletter">Newsletter</Link> |{' '}
-            <Link to="/giveaways/giveaway1">Giveaway1</Link> |{' '}
-            <Link to="/hereiswhere">HereIsWhere</Link>
-          </nav>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/terms" element={<Terms />} />
-            <Route path="/privacy" element={<Privacy />} />
-            <Route path="/newsletter" element={<Newsletter />} />
-            <Route path="/giveaways/giveaway1" element={<Giveaway1 />} />
-            <Route path="/hereiswhere" element={<HereIsWhere />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/newsletter" element={<Newsletter />} />
+          <Route path="/giveaways/giveaway1" element={<Giveaway1 />} />
+          <Route path="/hereiswhere" element={<HereIsWhere />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </BrowserRouter>
     </HelmetProvider>
   </StrictMode>
